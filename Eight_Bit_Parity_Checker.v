bbf217eee3608e57e5a8f43a0181dadf0818b95a
Sim Nabong <62924194+SimNabong@users.noreply.github.com>
2024-05-16 11:41:31 -0400

    Update Eight_Bit_Parity_Checker.v

--- Eight_Bit_Parity_Checker.v
+++ Eight_Bit_Parity_Checker.v
@@ -1,21 +1,8 @@
 //Parity Bit checker that checks if the data with a given parity bit matches its parity bit
 module Eight_Bit_Parity_Checker(
 	input [8:0]data, //9 bits with the data[8](the MSB) being the parity bit
-	input clk,
 	output [7:0]DataOut //outputs the 8-bits if parity matches
 );
-
-	reg [7:0] datar; //pipeline reg
-	reg PPAss;
-	always@(posedge clk) //pipeline
-		datar <= data[7:0]; 
-		
-	always@(*) begin
-		PPAss = (^data[7:0] == data[8])?1'b1:1'b0;
-	end
-
-	assign DataOut = (PPAss)? datar:8'd0;
-	
-
+	assign DataOut = (^data[7:0] == data[8])?data[7:0]:8'd0;
 	
 endmodule
