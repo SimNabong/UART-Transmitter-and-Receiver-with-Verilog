d1cf82482ddc794b5ce5a4ec5bd8e3faa1864907
Sim Nabong <62924194+SimNabong@users.noreply.github.com>
2024-05-25 07:33:58 -0400

    Update UARTReceiver.v
    changed logic generated clock to enable

--- UARTReceiver.v
+++ UARTReceiver.v
@@ -7,9 +7,8 @@ module UARTReceiver(
 	input [2:0]BC, //user baud rate control selection (~B3&~B2&B1)?9'd217:(~B3&B2&~B1)?9'd109:(~B3&B2&B1)?9'd72:(B3&~B2&~B1)?9'd36:9'd434;
 	output [7:0]DataOut, //8 output bits based on the transmitters input
 	output Mreset, //sends a reset signal to the transmitter
-	output Rx_clkw
+	output ena
 );
-
 	/*
 	50Mhz/bps=clkfrequency/bps
 	parameter Baud_9600 = 9'd434; //9600 bauds * 12 = 112500 bps;
@@ -20,21 +19,28 @@ module UARTReceiver(
 	*/			
 	
 	
-	//wire Rx_clkw;
+	wire enaw;
 	
-	reg Rx_inr,Rx_inr1,Rx_inr2,Rx_inr3;
+	reg Rx_inr=1'b0;
+	reg Rx_inr1;
 	
 	wire [8:0]dataw;
 	
+	assign ena = enaw;
 	
-	always@(posedge Rx_clkw) begin
-		Rx_inr1 <= Rx_in;
-		Rx_inr <= Rx_inr1;
+	always@(posedge clk) begin
+		if(enaw)
+			Rx_inr1 <= Rx_in;
 	end
 	
-	BaudControl BaudControlIns(.clk(clk), .BC(BC), .Rx_clk(Rx_clkw)); //Rx_clk is a derived clk from the 50MHz clk
+	always@(posedge clk) begin
+		if(enaw)
+			Rx_inr <= Rx_inr1;
+	end
 	
-	UARTReceiverStateMachine UARTReceiverStateMachineInst(.Rx_in(Rx_inr), .clk(Rx_clkw), .reset(reset), .Dout(dataw), .Mreset(Mreset));//outputs a slow pulse of data(dataw) to a sync before going to a parity bit checker
+	BaudControl BaudControlIns(.clk(clk), .BC(BC), .ena(enaw)); //ena is a derived clk from the 50MHz clk
+	
+	UARTReceiverStateMachine UARTReceiverStateMachineInst(.Rx_in(Rx_inr), .clk(clk), .ena(enaw), .reset(reset), .Dout(dataw), .Mreset(Mreset));//outputs a slow pulse of data(dataw) to a sync before going to a parity bit checker
 	
 	Eight_Bit_Parity_Checker Eight_Bit_Parity_Checker_Inst(.data(dataw), .DataOut(DataOut)); //parity checker that outputs the 8 bit data	
 	
